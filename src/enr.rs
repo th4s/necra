7a7c8310b2fa12eec4bbd63c756047bdabaf6eb5
th4s <th4s@metavoid.xyz>
2021-09-28 17:49:00 +0200

    Added sign method for identity scheme v4

--- src/enr.rs
+++ src/enr.rs
@@ -1,20 +1,36 @@
+use k256::ecdsa::{signature::Signer, Signature, SigningKey};
 use k256::CompressedPoint;
+use std::convert::AsRef;
 
+#[derive(Debug, Copy, Clone, Default)]
 pub struct EthereumNodeRecord {
     pub id: IdentityScheme,
     pub secp256k1: CompressedPoint,
-    pub ip: [u8; 4],
-    pub tcp: u16,
-    pub udp: u16,
-    pub ip6: [u8; 16],
-    pub tcp6: u16,
-    pub udp6: u16,
+    pub ip: Option<[u8; 4]>,
+    pub tcp: Option<u16>,
+    pub udp: Option<u16>,
+    pub ip6: Option<[u8; 16]>,
+    pub tcp6: Option<u16>,
+    pub udp6: Option<u16>,
 }
 
-impl EthereumNodeRecord {
-    fn builder(id: IdentityScheme) {}
-}
-
+#[derive(Debug, Copy, Clone, Default)]
 pub enum IdentityScheme {
+    #[default]
     V4,
+}
+
+impl std::fmt::Display for IdentityScheme {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "v4")
+    }
+}
+
+impl IdentityScheme {
+    pub fn sign(&self, key: &SigningKey, bytes: impl AsRef<[u8]>) -> Signature {
+        let signature: Signature = match self {
+            IdentityScheme::V4 => key.sign(bytes.as_ref()),
+        };
+        signature
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
-mod enr;
+#![feature(derive_default_enum)]
+
+pub mod enr;
